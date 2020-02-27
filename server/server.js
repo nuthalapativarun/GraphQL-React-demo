ba09443cd51beb6b8cc5e203a4569c0d3c87bc6f
Varun Nuthalapati <nuthalapativarun@gmail.com>
2020-02-26 22:07:17 -0800

    Server changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb+srv://nvarun123:varun123@lyricalgql-wiso9.mongodb.net/test?retryWrites=true&w=majority'
+const MONGO_URI = '';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
